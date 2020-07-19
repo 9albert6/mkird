f808cc4541a434acd71ce6aa06b62235c8a74a9d
9albert6 <9albert6@gmail.com>
2020-07-19 19:31:13 +0200

    Normalized data

--- weather_api.py
+++ weather_api.py
@@ -25,9 +25,22 @@ def delete_mostly_nan_columns(df):
         if name in df.columns:
             del df[name]
 
+def normalize_particular_columns(df, column_names):
+    for col_name in column_names:
+        df[col_name]=(df[col_name]-df[col_name].mean())/df[col_name].std()
+
 def get_data():    
     columns_names = read_column_names('./data/header.txt')
     df_smt = pd.read_csv('./data/s_m_t_2020.csv', names=columns_names[1:], index_col=False)
 
-    print(df_smt.sample(10))
+    delete_mostly_nan_columns(df_smt)
+    columns_to_normalize = ['zachmurzenie [oktanty]', 'prędkość wiatru [m/s]', 'temperatura [st C]',
+       'ciśnienie pary wodnej [hPa]', 'wilgotność względna [%]',
+       'ciśnienie na poziomie stacji [hPa]',
+       'ciśnienie na pozimie morza [hPa]', 'Suma opadu dzień [mm]',
+       'Suma opadu noc [mm]']
+    normalize_particular_columns(df_smt, columns_to_normalize)
+    print(df_smt.head(10))
     return df_smt
+
+get_data()
