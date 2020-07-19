40f799a5cf35740c19be8c66673e38573e63e8da
9albert6 <9albert6@gmail.com>
2020-07-19 17:17:52 +0200

    Deleted redundat columns

--- weather_api.py
+++ weather_api.py
@@ -12,10 +12,22 @@ def read_column_names(file):
     
     return [x.strip() for x in content]
 
+def delete_mostly_nan_columns(df):
+    column_names = list()
+    all_entries = len(df.index)
+    nan_values = df.isnull().sum().to_numpy()
+
+    for i in range(len(nan_values)):
+        if (nan_values[i]/all_entries)>=0.3:
+            column_names.append(df.columns[i])
+
+    for name in column_names:
+        if name in df.columns:
+            del df[name]
+
 def get_data():    
     columns_names = read_column_names('./data/header.txt')
     df_smt = pd.read_csv('./data/s_m_t_2020.csv', names=columns_names[1:], index_col=False)
 
+    print(df_smt.sample(10))
     return df_smt
-
-get_data()
