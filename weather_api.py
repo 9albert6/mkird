86df2b9abb68997efb803311ca1a573f35c6b542
9albert6 <9albert6@gmail.com>
2020-07-19 13:03:36 +0200

    Due to problem with encoding, the data is added staticly in repo.

--- weather_api.py
+++ weather_api.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 
-import urllib.request
 import pandas as pd
-from pathlib import Path
-import zipfile
 
-SITE_URL_SYNOP = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/synop/2020/2020_m_s.zip"
-SITE_URL_HEADER = "https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/miesieczne/synop/s_m_t_format.txt"
+__all__ = ["get_data"]
 
+DATA_FOLDER = "./data"
 
-def get_data():
-    Path("./data").mkdir(parents=True, exist_ok=True)
-    urllib.request.urlretrieve(SITE_URL_SYNOP, './data/2020_m_s.zip')
-    urllib.request.urlretrieve(SITE_URL_HEADER, './data/header.txt')
-
-
-    with zipfile.ZipFile('./data/2020_m_s.zip', 'r') as zip_ref:
-        zip_ref.extractall('./data')
-
-    df_smt = pd.read_csv('./data/s_m_t_2020.csv', encoding='iso-8859-2', header=None)
-
-    print(df_smt.sample(10))
+def read_column_names(file):
+    with open(file, 'r') as f:
+        content = f.readlines()
     
+    return [x.strip() for x in content]
+
+def get_data():    
+    columns_names = read_column_names('./data/header.txt')
+    df_smt = pd.read_csv('./data/s_m_t_2020.csv', names=columns_names[1:], index_col=False)
+
     return df_smt
+
 get_data()
