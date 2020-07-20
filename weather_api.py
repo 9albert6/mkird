b12e553f47831e2957605d2139b2336c8e35cc3f
Justyna <49339762+JustynaKloc@users.noreply.github.com>
2020-07-20 13:57:39 +0200

    Change columns name to columns index

--- weather_api.py
+++ weather_api.py
@@ -27,20 +27,14 @@ def delete_mostly_nan_columns(df):
 
 def normalize_particular_columns(df, column_names):
     for col_name in column_names:
-        df[col_name]=(df[col_name]-df[col_name].mean())/df[col_name].std()
+        df.iloc[:,col_name]=(df.iloc[:,col_name]-df.iloc[:,col_name].mean())/df.iloc[:,col_name].std()
 
 def get_data():    
     columns_names = read_column_names('./data/header.txt')
     df_smt = pd.read_csv('./data/s_m_t_2020.csv', names=columns_names[1:], index_col=False)
 
     delete_mostly_nan_columns(df_smt)
-    columns_to_normalize = ['zachmurzenie [oktanty]', 'prędkość wiatru [m/s]', 'temperatura [st C]',
-       'ciśnienie pary wodnej [hPa]', 'wilgotność względna [%]',
-       'ciśnienie na poziomie stacji [hPa]',
-       'ciśnienie na pozimie morza [hPa]', 'Suma opadu dzień [mm]',
-       'Suma opadu noc [mm]']
+    columns_to_normalize = [[4],[5],[6],[7],[8],[9],[10],[11],[12]]
     normalize_particular_columns(df_smt, columns_to_normalize)
     print(df_smt.head(10))
     return df_smt
-
-get_data()
