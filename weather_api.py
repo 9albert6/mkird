151fdbea957ac157c9950c57f7141fa9226a71a0
Justyna <jkloc96@gmail.com>
2020-08-05 16:11:13 +0200

    I've done a lot...

--- weather_api.py
+++ weather_api.py
@@ -6,7 +6,7 @@ import shlex
 from platform import system
 import sys
 
-__all__ = ["get_data"]
+__all__ = ["get_data","read_column_names"]
 
 DATA_FOLDER = "./data"
 
@@ -16,18 +16,18 @@ def read_column_names(file):
     
     return [x.strip() for x in content]
 
-# def delete_mostly_nan_columns(df):
-#     column_names = list()
-#     all_entries = len(df.index)
-#     nan_values = df.isnull().sum().to_numpy()
+def delete_mostly_nan_columns(df):
+    column_names = list()
+    all_entries = len(df.index)
+    nan_values = df.isnull().sum().to_numpy()
 
-#     for i in range(len(nan_values)):
-#         if (nan_values[i]/all_entries)>=0.3:
-#             column_names.append(df.columns[i])
+    for i in range(len(nan_values)):
+        if (nan_values[i]/all_entries)>=0.3:
+            column_names.append(df.columns[i])
 
-#     for name in column_names:
-#         if name in df.columns:
-#             del df[name]
+    for name in column_names:
+        if name in df.columns:
+            del df[name]
 
 def normalize_particular_columns(df, column_names):
     for col_name in column_names:
@@ -40,10 +40,10 @@ def get_data(city_name):
     
     subprocess.call(shlex.split(f'./get_files.sh {city_name}'))
     df_smt = pd.read_csv(f'./data/s_m_t_{city_name}.csv', names=columns_names[1:], index_col=False)
-#     delete_mostly_nan_columns(df_smt)
+    delete_mostly_nan_columns(df_smt)
 #     columns_to_normalize = ['Średnia dobowa temperatura [°C]', 'Status pomiaru TEMP',
 #        'Status pomiaru WLGS', 'Średnia dobowa prędkość wiatru [m/s]',
 #        'Status pomiaru FWS', 'Średnie dobowe zachmurzenie ogólne [oktanty]',
 #        'Status pomiaru NOS']
-    normalize_particular_columns(df_smt, columns_to_normalize)
+    #normalize_particular_columns(df_smt, columns_to_normalize)
     return df_smt
