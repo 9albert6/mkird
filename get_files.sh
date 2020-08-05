e18a529e8f249edda4da10b2f4acf776f2b84bbc
Justyna <jkloc96@gmail.com>
2020-08-05 15:47:50 +0200

    Du hast!

--- get_files.sh
+++ get_files.sh
@@ -19,7 +19,7 @@ if [ -f "$MAIN_FILE" ] ; then
 fi
 
 #download all zipped data into ./temp forder
-wget -q -r -l1 --no-directories --no-parent -A "*.zip" https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/klimat/20{19..20}/ -P ./temp/
+wget -q -r -l1 --no-directories --no-parent -A "*.zip" https://dane.imgw.pl/data/dane_pomiarowo_obserwacyjne/dane_meteorologiczne/dobowe/synop/20{19..20}/ -P ./temp/
 
 #unzip all
 for FILE in ./temp/*.zip
@@ -28,13 +28,13 @@ do
 done
 
 #remove zipped fordels and unnecessairy csv files
-rm ./temp/*.zip ./temp/k_d_[0-9]*.csv
+rm ./temp/*.zip ./temp/s_d_[0-9]*.csv
 
 #concatenate all data to signle file
 cat ./temp/*.csv > ./data/s_m_t.csv
 
 #remove temp file
-rm -rf ./temp/
+#rm -rf ./temp/
 
 #get data of single city
 grep -i $1 ./data/s_m_t.csv > ./data/s_m_t_$1.csv
